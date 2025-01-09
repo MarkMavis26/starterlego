094f27ba7fdb2ac7f3e9bc3839fd498ac8b2ff76
Mark Mavis <legomavrock@gmail.com>
2025-01-08 21:00:46 -0600

    things mostly working end to end

--- rover/flask/rover.py
+++ rover/flask/rover.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
+from bokeh.plotting import figure, show
+from bokeh.embed import components
 
+import math
+import json
+import random
 app = Flask(__name__)
 
 # In-memory database (for simplicity)
 missions = {}
 telemetry = {}
 current_mission_id = 1
+
+
 
 @app.route('/missions', methods=['GET'])
 def get_missions():
@@ -25,10 +32,28 @@ def create_mission():
     """Create a new mission."""
     global current_mission_id
     mission_data = request.get_json()
+    
+    # TODO - introduce Mission class
+    #
+    #   mission = new Mission(current_mission_id)
+    #
     mission_data['id'] = current_mission_id
+    mission_data['x_coord'] = 0
+    mission_data['y_coord'] = 0
+
     missions[current_mission_id] = mission_data
     current_mission_id += 1
+
     return jsonify({"id": mission_data['id']}), 201
+
+def process_telemetry(mission, telemetry_data):
+  
+    moved_cm = telemetry_data['moved_cm']
+    current_angle = math.radians(telemetry_data['current_angle'])
+    mission['x_coord'] = mission['x_coord'] + (math.cos(current_angle) * moved_cm)
+    mission['y_coord'] = mission['y_coord'] + (math.sin(current_angle) * moved_cm)
+    telemetry_data['x_coord'] = mission['x_coord']
+    telemetry_data['y_coord'] = mission['y_coord']
 
 @app.route('/missions/<int:id>/telemetry', methods=['POST'])
 def send_telemetry(id):
@@ -39,9 +64,31 @@ def send_telemetry(id):
     telemetry_data = request.get_json()
     if id not in telemetry:
         telemetry[id] = []
-    
+
+    # TODO - extract the code below into a function so we can use it here, AND from our seed_data function  
+      
+    mission = missions.get(id)
+    process_telemetry(mission, telemetry_data)
     telemetry[id].append(telemetry_data)
+
+    print("Missions")
+    print(json.dumps(missions, indent=4))
+    print("Telemetry")
+    print(json.dumps(telemetry, indent=4))
+
     return jsonify({"message": "Telemetry received"}), 200
+
+
+@app.route('/missions/<int:id>/telemetry', methods=['GET'])
+def list_telemetry(id):
+    """List all telemetry for a given mission."""
+    if id not in missions:
+        return jsonify({"error": "Mission not found"}), 404
+    
+    # Retrieve telemetry data for the mission
+    mission_telemetry = telemetry.get(id, [])
+    return jsonify({"telemetry": mission_telemetry}), 200
+
 
 @app.route('/missions/<int:id>/instruction', methods=['GET'])
 def get_instruction(id):
@@ -54,6 +101,63 @@ def get_instruction(id):
         "instruction": "Move forward 1 meter"  # Example instruction
     }
     return jsonify(instruction), 200
+
+#UI Below
+@app.route('/ui/missions/<int:id>')
+def mission_telemetry(id):
+    if id not in telemetry:
+        return render_template('telemetry.html', mission_name="Unknown Mission", script="", div="")
+    
+    mission_name = "test" #missions[id]["name"]
+    print(json.dumps(telemetry[id], indent=4))
+    coordinates = telemetry[id]
+    x_coords = [c["x_coord"] for c in coordinates]
+    y_coords = [c["y_coord"] for c in coordinates]
+
+    # Create a Bokeh plot with Cartesian coordinates
+    plot = figure(title=f"Telemetry for {mission_name}", x_axis_label="X", y_axis_label="Y")
+    plot.circle(x_coords, y_coords, size=10, color="blue", legend_label="Telemetry Points")
+
+    # return show(plot)
+
+    # # Embed the plot into the page
+    script, div = components(plot)
+
+    return render_template('telemetry.html', mission_name=mission_name, script=script, div=div)
+
+@app.route('/ui/missions', methods=['GET'])
+def home():
+    return render_template('index.html', missions=missions)
+
+def seed_data():
+    ("Seeding data")
+    global current_mission_id
+    for i in range(1):
+        mission_data = {}
+
+        mission_data['id'] = current_mission_id
+        mission_data['name'] = "Mission {}".format(current_mission_id)
+        mission_data['x_coord'] = 0 
+        mission_data['y_coord'] = 0
+
+        missions[current_mission_id] = mission_data
+     
+
+        # create sample telemetry
+        # - add X number of telemetry records with randomized x/y coords (ask chat GPT how to generate random values)
+        # - also randomize angle (same way, 0-360)
+        telemetry[current_mission_id] = []
+        for i in range(10):
+            telemetry_data = {}
+            telemetry_data['distance_in_cm'] = 0
+            telemetry_data['moved_cm'] = random.random()
+            telemetry_data['move_rot'] = 0
+            telemetry_data['current_angle'] = random.randint(1, 360)
+            process_telemetry(mission_data, telemetry_data)
+            telemetry[current_mission_id].append(telemetry_data)
+        current_mission_id += 1
+
+seed_data()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
--- rover/robot/mapper.py
+++ rover/robot/mapper.py
@@ -18,15 +18,17 @@ from pybricks.robotics import DriveBase
 from pybricks.nxtdevices import UltrasonicSensor
 import logging
 import urequests
+import ujson
 
 ev3 = EV3Brick()
 logging.basicConfig(
     level=logging.INFO,  # Set to DEBUG for more details
     format="%(asctime)s [%(levelname)s] %(message)s",
 )
+print("debug 1")
 left_motor = Motor(Port.C)
 right_motor = Motor(Port.B)
-ultrasonic_sensor = UltrasonicSensor(Port.S1)
+ultrasonic_sensor = UltrasonicSensor(Port.S2)
 
 def move(m: Motor, s: int, r: int, w: bool):
     m.run_angle(speed=s, rotation_angle=r, wait=w)
@@ -35,42 +37,81 @@ def forward(rot_angle_cm: int):
     move(left_motor, 200, rot_angle_cm, False)    
     move(right_motor, 200, rot_angle_cm, True)   
 
-def pivot_right(move_angle: int): 
-    move(left_motor, 200, move_angle, False)    
-    move(right_motor, 200, -move_angle, True)    
+def pivot_right(rotation_angle: int): 
+    move(left_motor, 200, rotation_angle, False)    
+    move(right_motor, 200, -rotation_angle, True)
+    #todo: confirm rotation angle    
+    (rotation_angle/500)*90
 
-def pivot_left(move_angle: int):
-    move(right_motor, 200, move_angle, False)    
-    move(left_motor, 200, -move_angle, True)
+def pivot_left(rotation_angle: int):
+    move(right_motor, 200, rotation_angle, False)    
+    move(left_motor, 200, -rotation_angle, True)
+    #todo: confirm rotation angle    
+    -(rotation_angle/500)*90
 
 def backward(rot_angle_cm: int): 
     forward(-rot_angle_cm)
 
+def send_telemetry(distance_in_centimeters, rot_angle_cm, current_angle, moved_cm):
+    try:
+        telemetry_data = {
+            "distance_in_cm": distance_in_centimeters,
+            "move_rot": rot_angle_cm,
+            "current_angle": current_angle,
+            "moved_cm": moved_cm
+        }
+        print(ujson.dumps(telemetry_data))
+        telemetry_response = urequests.post("{}/missions/{}/telemetry".format(server, missionID), json=telemetry_data, headers=headers)
+    except Exception as e:
+        logging.error("An unexpected error occurred: %s", e)       
+
+    
 def main():
     targ = range(10,100000)
     rot_angle_cm=31.83098863
     #each rotation moves the bot forward approx 5cm
-    move_angle=500
-
+    print("debug 3")
+   
+    server = "http://192.168.4.69:5001"
+    print("debug 4")
     #create mission against flask server
     headers = {'Content-Type': 'application/json'}
-    r = urequests.post('http://192.168.4.69:5001/missions', json={}, headers=headers)
-    logging.info("response: {}".format(r.content))
-
+    print("debug 4")
+    payload = ujson.dumps({})
+    print("debug 4")
+    #https://www.ev3dev.org/docs/tutorials/setting-up-wifi-using-the-command-line/
+    r = urequests.post("{}/missions".format(server), data=payload, headers=headers)
+    print("debug 5")
+    # missionID = r.json()['id']
+    
+    response_data = r.json()
+    missionID = response_data['id']
+    print("Response:", response_data)
+    print("Mission ID:", missionID) 
+  
+    # logging.info("response: {}, missionID: {}".format(r.content, missionID))
+    current_angle = 0
+    print("debug 6")
+    #breakpoint()
+    
     while True:
         forward(rot_angle_cm)
         distance_in_centimeters = ultrasonic_sensor.distance()/10
         logging.info("Distance: {} cm".format(distance_in_centimeters))
-
-        # TEMP - just make http request
-        
-        
-
+        print("debug 3")
+        send_telemetry(distance_in_centimeters, rot_angle_cm, current_angle, 1)
+       
        #IF LAST MEASURE LESS THAN 200 AND NEXT MEASURE IS GREATER THAN 2000, ASSUME YOU'VE HIT A WALL
+
         while distance_in_centimeters not in targ:
             backward(rot_angle_cm)
-            logging.info("pivoting right {}".format(move_angle))
-            pivot_right(move_angle)
+            send_telemetry(distance_in_centimeters, rot_angle_cm, current_angle, -1)
+
+            wheel_rotation_angle=500
+            logging.info("pivoting right {}".format(wheel_rotation_angle))
+
+            current_angle+=pivot_right(wheel_rotation_angle)
+            
             distance_in_centimeters = ultrasonic_sensor.distance()
         
 if __name__ == "__main__":
